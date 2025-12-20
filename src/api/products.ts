61663a2ddaa91ddb48ffc28decf8bc34942422b9
d3haka <arian.rassaee@gmail.com>
2025-12-20 21:06:24 +0330

    minor refactors

--- src/api/products.ts
+++ src/api/products.ts
@@ -1,4 +1,4 @@
-import { Product } from "./types/product";
+import { Product } from "@/types/product";
 
 export type ProductsResData = {
   products: Product[];
---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div dir="rtl" className="flex h-screen items-center justify-center bg-background">
+      صفحه‌ی مورد نظر پیدا نشد.
+    </div>
+  );
+}
